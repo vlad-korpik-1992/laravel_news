61aebba034544c7b905cd392807a54606f7e3d6e
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-11-30 16:48:04 +0300

    Update and SoftDeletes post

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
 
 class EditController extends Controller
 {
     public function __invoke(Post $post)
     {
-        return view('admin.post.edit', compact('post'));
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.edit', compact('post','categories','tags'));
     }
 }
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -27,16 +27,77 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
             <div class="col-12">
-                <form class="w-25" action="{{ route('admin.post.update', $post->id) }}" method="POST">
+                <form class="w-100" action="{{ route('admin.post.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method('PATCH')
                     <div class="form-group">
-                        <input class="form-control" type="text" name="title" value="{{ $post->title }}" placeholder="Название поста">
+                        <input class="form-control w-25" type="text" name="title" placeholder="Название поста" value="{{ $post->title }}">
+                        @error('title')
+                            <div class="text-danger">Это поле обязательно для заполнения</div>
+                        @enderror
                     </div>
-                    @error('title')
-                        <div class="text-danger">Это поле обязательно для заполнения</div>
-                    @enderror
-                    <input class="btn btn-block btn-primary w-50" type="submit" value="Обновить">
+                    <div class="form-group">
+                        <textarea id="summernote" name="content">{{ $post->content }}</textarea>
+                        @error('content')
+                            <div class="text-danger">Это поле обязательно для заполнения</div>
+                        @enderror
+                    </div>
+                    <div class="form-group w-50">
+                        <label for="exampleInputFile">Добавить превью</label>
+                        <div class="w-25 mb-3">
+                            <img class="w-100" src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image">
+                        </div>
+                        <div class="input-group">
+                          <div class="custom-file">
+                            <input name="preview_image" type="file" class="custom-file-input">
+                            <label class="custom-file-label">Выберите изображение</label>
+                          </div>
+                          <div class="input-group-append">
+                            <span class="input-group-text">Загрузить</span>
+                          </div>
+                        </div>
+                        @error('preview_image')
+                            <div class="text-danger">Это поле обязательно для заполнения</div>
+                        @enderror
+                    </div>
+                    <div class="form-group w-50">
+                        <label for="exampleInputFile">Добавить главное изображение</label>
+                        <div class="w-25 mb-3">
+                            <img class="w-100" src="{{ url('storage/' . $post->main_image) }}" alt="main_image">
+                        </div>
+                        <div class="input-group">
+                          <div class="custom-file">
+                            <input name="main_image" type="file" class="custom-file-input">
+                            <label class="custom-file-label">Выберите изображение</label>
+                          </div>
+                          <div class="input-group-append">
+                            <span class="input-group-text">Загрузить</span>
+                          </div>
+                        </div>
+                        @error('preview_image')
+                            <div class="text-danger">Это поле обязательно для заполнения</div>
+                        @enderror
+                    </div>
+                    <div class="form-group w-25">
+                        <label>Выберите категорию</label>
+                        <select name="category_id" class="form-control">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"
+                                {{ $category->id == $post->category_id ? 'selected' : '' }}>
+                                    {{ $category->title }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group w-75">
+                        <label>Теги</label>
+                        <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите теги" style="width: 100%;">
+                            @foreach ($tags as $tag)
+                                <option {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : '' }} value = "{{$tag->id}}">{{ $tag->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <input class="btn btn-block btn-primary w-25" type="submit" value="Обновить">
                 </form>
             </div>
         </div>
