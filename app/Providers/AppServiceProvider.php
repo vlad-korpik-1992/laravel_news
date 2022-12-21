0e50f20446cf0dd24fcf2e5bdf1bf0bb1249b262
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-12-21 16:13:04 +0300

    Added post page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Carbon::setLocale('ru_RU');
         Paginator::useBootstrap();
     }
 }
