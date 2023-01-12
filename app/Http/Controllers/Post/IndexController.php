add25d23d10e1e7c8201cc04ac5b4d7b5c4a2f96
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-01-12 16:40:43 +0300

    Database query optimization

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
+        $posts = Post::with('category')->paginate(6);
         $randomPosts = Post::get()->random(4);
         $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(2);
         return view('post.index', compact('posts', 'randomPosts', 'likedPosts'));
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,6 +16,8 @@ class Post extends Model
 
     protected $withCount = ['likedUsers'];
 
+    // protected $with = ['category'];
+
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
